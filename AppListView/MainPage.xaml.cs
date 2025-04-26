034dba2dea28a9f0044a781adfddc7be4a4cf38f
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-04-25 21:07:21 -0300

    AppLIstView aula dia 25/04/2025

--- AppListView/MainPage.xaml.cs
+++ AppListView/MainPage.xaml.cs
@@ -44,7 +44,32 @@ namespace AppListView
 
         private void btnAdicionar_Clicked(object sender, EventArgs e)
         {
+            string nome = txtNome.Text;
+            string idade = txtIdade.Text;
 
+            //Valida simples se o campo ta preenchido
+            if(string.IsNullOrEmpty(nome) || 
+                string.IsNullOrEmpty(idade))
+            {
+                DisplayAlert("Atenção",
+                    "Preencha os campos corretamente.", "OK");
+                return; //para a excução do botão
+            }
+
+            //Se chegou até aqui, é pq esta tudo certo
+            //pode adicionar a lista
+
+            pessoas.Add(
+                    new Pessoa
+                    {
+                        Nome = nome,
+                        Idade = idade
+                    }
+                );
+
+            //Limpar os campos
+            txtNome.Text = "";
+            txtIdade.Text = "";
         }
     }
 }
