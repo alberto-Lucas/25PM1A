f7e9085caad701a7a97b5a362f9b53bc87121278
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-05-30 22:14:13 -0300

    AppMenuShell aula dia 30/05/2025

---
+++ AppMenuShell/Views/pgCadastro.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppMenuShell.Views;
+
+public partial class pgCadastro : ContentPage
+{
+	public pgCadastro()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppMenuShell/Views/pgInicial.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppMenuShell.Views;
+
+public partial class pgInicial : ContentPage
+{
+	public pgInicial()
+	{
+		InitializeComponent();
+	}
+}
